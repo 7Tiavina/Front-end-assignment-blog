882f0b127608292b61f323e880a6ef93f0ee4255
portableTiavina <setanirinatiavina@gmail.com>
2025-12-16 11:05:19 +0400

    Delete Postes

--- src/components/PosteDetail.jsx
+++ src/components/PosteDetail.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import '../styles/PosteDetail.css'
 
 function PosteDetail() {
   const { id } = useParams()
+  const navigate = useNavigate()
   const [poste, setPoste] = useState(null)
 
   useEffect(() => {
@@ -12,12 +13,28 @@ function PosteDetail() {
       .then(data => setPoste(data))
   }, [id])
 
+  const deletePoste = () => {
+    fetch(`http://localhost:8000/api/postes/${id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      navigate('/')
+    })
+  }
+
   if (!poste) return <p>Chargement...</p>
 
   return (
     <div className="poste-detail">
+      <button className="back-btn" onClick={() => navigate(-1)}>
+        -Retour
+      </button>
+
       <h1>{poste.title}</h1>
       <p>{poste.content}</p>
+
+      <button className="delete-btn" onClick={deletePoste}>
+        Supprimer l’article
+      </button>
     </div>
   )
 }
